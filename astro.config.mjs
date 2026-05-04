24015eff925c545551f99de1b6dd5bcb7a2d17da
Hamza GUEBAILIA <hamzaguebailia354@gmail.com>
2026-05-04 19:30:24 +0100

    Fix broken Netlify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,26 +2,9 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://gift-guides.netlify.app/',
-
-  integrations: [
-    sitemap({
-      filter: (page) => !page.includes('/admin'),
-      changefreq: 'weekly',
-      priority: 0.7,
-      lastmod: new Date(),
-    }),
-  ],
-
+  site: 'https://gift-guides.netlify.app',
   output: 'static',
-
-  build: {
-    assets: 'assets'
-  },
-
-  vite: {
-    ssr: {
-      external: ['svgo'],
-    },
-  },
+  integrations: [
+    sitemap()
+  ]
 });
